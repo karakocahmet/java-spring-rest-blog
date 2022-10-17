c08f211b4500e511098add3ff6a1da9f95e8ddef
ahmet karakoc <akarakoc2@sgk.gov.tr>
2022-10-17 14:14:04 +0300

    odev1-3

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+    @RestResource(rel="contains-title", path="containsTitle")
+    public List<Post>  findByTitleContaining(String title);
 }
