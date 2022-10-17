2f99f9274870fd52e777466447c12cce22b0d473
ahmet karakoc <akarakoc2@sgk.gov.tr>
2022-10-17 14:57:35 +0300

    odev2-4

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -2,11 +2,13 @@ package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RepositoryRestResource(exported = false)
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
 }
